281d26c89ca887b0d8be26b7d9fd31fad654cd93
Andrey Barsukov <andrey.bv.gm@gmail.com>
2021-12-30 13:55:52 +0500

    Реализовано скрытие/появление кнопок для прокрутки элементов дисплея

--- client/js/calculator/calculator.js
+++ client/js/calculator/calculator.js
@@ -500,12 +500,21 @@ const resizeItemsDisplayObserver = new ResizeObserver(entries => {
 	console.log(entries);
 	for (let item of entries) {
 		const wrapperItem = item.target.parentNode;
-		let marginLeft = parseInt(getComputedStyle(wrapperItem).marginLeft);
-		let marginRight = parseInt(getComputedStyle(wrapperItem).marginRight);
-		let widthItem = item.target.offsetWidth;
-		let widthContainer = wrapperItem.offsetWidth + marginLeft + marginRight;
+		const marginLeft = parseInt(getComputedStyle(wrapperItem).marginLeft);
+		const marginRight = parseInt(getComputedStyle(wrapperItem).marginRight);
+		const widthItem = item.target.offsetWidth;
+		const widthContainer = wrapperItem.offsetWidth + marginLeft + marginRight;
+		const wrapperControl = wrapperItem.parentNode;
+		const leftButton = wrapperControl.querySelector('.display-scroll-wrapper__left-button');
+		const rightButton = wrapperControl.querySelector('.display-scroll-wrapper__right-button');
+
 		if (widthItem > widthContainer) {
+			leftButton.classList.remove('none');
+			rightButton.classList.remove('none');
 			wrapperItem.scrollLeft = wrapperItem.scrollWidth - widthContainer;
+		} else {
+			leftButton.classList.add('none');
+			rightButton.classList.add('none');
 		}
 	}
 });
